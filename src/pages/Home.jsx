a998e9a0089d9cc26e63528253bb8348cdba8c06
zafarbek <zafarbektolanboyev618@gmail.com>
2024-09-30 09:49:39 +0500

    react-auth

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,14 +1,23 @@
-import React from 'react'
-import Cards from '../components/Cards'
-import AddProduct from '../components/AddProducts'
+import React from 'react';
+import { useNavigate } from 'react-router-dom'; // import from react-router-dom
+import Cards from '../components/Cards';
+import AddProduct from '../components/AddProducts';
 
 function Home() {
+  const navigate = useNavigate(); // useNavigate ni chaqirish
+
+  function handleClick(e) {
+    e.preventDefault();
+    navigate('/register'); // navigate funksiyasidan foydalanish
+  }
+
   return (
     <div>
-      <AddProduct></AddProduct>
-      <Cards></Cards>
+      <AddProduct />
+      <Cards />
+      <button onClick={handleClick}>Register</button>
     </div>
-  )
+  );
 }
 
-export default Home
+export default Home;
